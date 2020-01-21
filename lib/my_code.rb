9429ef1c63893f73cbda4ab928697eec3e0f93a4
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-21 18:48:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,21 +14,42 @@ end
 
 
 def map_to_no_change(source_array)
-  
+  result_array = []
+  index = 0
+  source_array.each do |num|
+    num = num * -1
+    result_array[index] = num
+    index += 1
+  end
+  result_array  
   
   
 end
 
 
 def map_to_double(source_array)
-  
+  result_array = []
+  index = 0
+  source_array.each do |num|
+    num = num * -1
+    result_array[index] = num
+    index += 1
+  end
+  result_array  
   
   
 end
 
 
 def map_to_square(source_array)
-  
+    result_array = []
+  index = 0
+  source_array.each do |num|
+    num = num * -1
+    result_array[index] = num
+    index += 1
+  end
+  result_array
   
   
 end
